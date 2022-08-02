7d6b9b010ead1ab060f572f84a3c995ba3da0ae9
Ulf Zibis <Ulf.Zibis@CoSoCo.de>
2022-08-02 20:57:01 +0200

    Completed brief install guide. Added uninstall libpisock-dev.sh.

---
+++ uninstall libpisock-dev.sh
@@ -0,0 +1,59 @@
+# ./usr:
+# insgesamt 12
+# drwxrwxrwx 1 root root 8192 Sep 17  2016 include
+# drwxrwxrwx 1 root root 4096 Sep 17  2016 lib
+# drwxrwxrwx 1 root root    0 Sep 17  2016 share
+rm /usr/include/pi-address.h
+rm /usr/include/pi-appinfo.h
+rm /usr/include/pi-args.h
+rm /usr/include/pi-blob.h
+rm /usr/include/pi-bluetooth.h
+rm /usr/include/pi-buffer.h
+rm /usr/include/pi-calendar.h
+rm /usr/include/pi-cmp.h
+rm /usr/include/pi-contact.h
+rm /usr/include/pi-datebook.h
+rm /usr/include/pi-debug.h
+rm /usr/include/pi-dlp.h
+rm /usr/include/pi-error.h
+rm /usr/include/pi-expense.h
+rm /usr/include/pi-file.h
+rm /usr/include/pi-foto.h
+rm /usr/include/pi-header.h
+rm /usr/include/pi-hinote.h
+rm /usr/include/pi-inet.h
+rm /usr/include/pi-location.h
+rm /usr/include/pi-macros.h
+rm /usr/include/pi-mail.h
+rm /usr/include/pi-md5.h
+rm /usr/include/pi-memo.h
+rm /usr/include/pi-money.h
+rm /usr/include/pi-net.h
+rm /usr/include/pi-notepad.h
+rm /usr/include/pi-padp.h
+rm /usr/include/pi-palmpix.h
+rm /usr/include/pi-serial.h
+rm /usr/include/pi-slp.h
+rm /usr/include/pi-sockaddr.h
+rm /usr/include/pi-socket.h
+rm /usr/include/pi-source.h
+rm /usr/include/pi-sync.h
+rm /usr/include/pi-sys.h
+rm /usr/include/pi-syspkt.h
+rm /usr/include/pi-threadsafe.h
+rm /usr/include/pi-todo.h
+rm /usr/include/pi-usb.h
+rm /usr/include/pi-util.h
+rm /usr/include/pi-veo.h
+rm /usr/include/pi-versamail.h
+rm /usr/include/pi-version.h
+rm /usr/lib/libpisock.a
+rm /usr/lib/libpisock.la
+rm /usr/lib/libpisock.so
+rm /usr/lib/libpisync.a
+rm /usr/lib/libpisync.la
+rm /usr/lib/libpisync.so
+rm /usr/lib/pkgconfig/pilot-link.pc
+rm /usr/share/aclocal/pilot-link.m4
+rm -r /usr/share/doc/libpisock-dev/
+
